d61d59e2bc546898fbb00c77c8c388aa611ba6cb
Russel <blokhin.rus@gmail.com>
2021-05-17 19:14:00 +0300

    bags

---
+++ index.js
@@ -0,0 +1,50 @@
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.refs = {
+      dateInput: document.querySelector("#dateInput"),
+      timeInput: document.querySelector("#timeInput"),
+      timer: document.querySelector(selector),
+    };
+    this.intervalId = null;
+  }
+  render(days, hours, mins, secs) {
+    this.refs.timer.querySelector("[data-value='days']").textContent = days;
+    this.refs.timer.querySelector("[data-value='hours']").textContent = hours;
+    this.refs.timer.querySelector("[data-value='mins']").textContent = mins;
+    this.refs.timer.querySelector("[data-value='secs']").textContent = secs;
+  }
+  startCountdown() {
+    if (this.intervalId) {
+      return;
+    }
+
+    const time = Date.parse(this.targetDate) - Date.parse(new Date());
+
+    this.intervalId = setInterval(() => {
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+      const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+      this.render(days, hours, mins, secs);
+      // this.refs.timer.querySelector("[data-value='days']").textContent = days;
+      // this.refs.timer.querySelector("[data-value='hours']").textContent = hours;
+      // this.refs.timer.querySelector("[data-value='mins']").textContent = mins;
+      // this.refs.timer.querySelector("[data-value='secs']").textContent = secs;
+    }, 1000);
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Jul 17, 2021"),
+});
+
+const startBtn = document.querySelector("#startBtn");
+
+startBtn.addEvenListener("click", timer.startCountdown.bind(timer));
+console.log(startBtn);

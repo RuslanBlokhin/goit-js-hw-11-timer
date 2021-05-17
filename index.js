af1ab9a60141adc05493ad342e8759fa69450585
Russel <blokhin.rus@gmail.com>
2021-05-17 22:03:10 +0300

    complete

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const startBtn = document.querySelector("#startBtn");
+const dateInputRef = document.querySelector("#dateInput");
+const timeInputRef = document.querySelector("#timeInput");
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
@@ -15,36 +18,26 @@ class CountdownTimer {
     this.refs.timer.querySelector("[data-value='mins']").textContent = mins;
     this.refs.timer.querySelector("[data-value='secs']").textContent = secs;
   }
-  startCountdown() {
-    if (this.intervalId) {
-      return;
-    }
-
+  timer() {
     const time = Date.parse(this.targetDate) - Date.parse(new Date());
-
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    this.render(days, hours, mins, secs);
+  }
+  startCountdown() {
+    if (this.intervalId) return;
+    this.timer();
     this.intervalId = setInterval(() => {
-      const days = Math.floor(time / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-      const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-      this.render(days, hours, mins, secs);
-      // this.refs.timer.querySelector("[data-value='days']").textContent = days;
-      // this.refs.timer.querySelector("[data-value='hours']").textContent = hours;
-      // this.refs.timer.querySelector("[data-value='mins']").textContent = mins;
-      // this.refs.timer.querySelector("[data-value='secs']").textContent = secs;
+      this.timer();
     }, 1000);
   }
 }
-
-const timer = new CountdownTimer({
-  selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2021"),
+startBtn.addEventListener("click", function () {
+  const date = dateInputRef.value + " " + timeInputRef.value;
+  new CountdownTimer({
+    selector: "#timer-1",
+    targetDate: new Date(date),
+  }).startCountdown();
 });
-
-const startBtn = document.querySelector("#startBtn");
-
-startBtn.addEvenListener("click", timer.startCountdown.bind(timer));
-console.log(startBtn);
